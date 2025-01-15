fea5f39d561259ef392c5fe5d432f93fab500d3a
Abdullah <abdullahrehan2010@gmail.com>
2025-01-15 18:53:17 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+
+num1 = int(input("Please ENter First  Number = "))
+num2 = int(input("Please ENter Second  Number = "))
+
+
+print ("num1 & num2=", num1 & num2)
+
+print("\nnum1 | num2=", num1 | num2)
+
+print ("\n~num1 =", ~num1)
+
+print("\nnum1 ^ num2 =", num1 ^ num2)
+
+print("\nnum1 >> num2 =", num1 >> num2)
+
+print("\nnum1 << num2 =", num1 << num2)
+
+
+
